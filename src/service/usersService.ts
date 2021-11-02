faad32e7ef1b836ce5bafe2b2502e1681b1a2101
HenriqueMiguelAnjos <henrique.anjos@dock.tech>
2021-11-02 00:46:32 -0300

    Tratando com async await

--- src/service/usersService.ts
+++ src/service/usersService.ts
@@ -1,16 +1,13 @@
 import axios from 'axios'
 
 async function getUsers(username: string) {
-    return new Promise(async (resolve, reject) => {
-        try {
-            const result = await axios.get(`https://api.github.com/users/${username}`)
-            resolve(result.data)
-        }
-        catch (err) {
-            console.log(err);
-            reject(err);
-          }    
-    })
+    try {
+        const result = await axios.get(`https://api.github.com/users/${username}`)
+        return result.data
+    }
+    catch (err) {
+        console.log(err);
+    }
 }
 
 export {
