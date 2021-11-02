f348ff49c8ee3f2f48259bf81e87505e0d69ccda
HenriqueMiguelAnjos <henrique.anjos@dock.tech>
2021-11-02 00:50:08 -0300

    Usando Async await

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -6,9 +6,9 @@ const router: Router = express.Router();
 router.get('/', async (req: express.Request, res: express.Response) => {
 
     const userName = req.body.name
-    await getUsers(userName)
-        .then(result => res.status(200).send(result).end())
-        .catch(err => res.status(400).send(`Não foi possível receber dados do usuário ${err.message}`).end());
+    const result = await getUsers(userName)
+
+    return res.status(200).send(result).end()
 })
 
 module.exports = router;
